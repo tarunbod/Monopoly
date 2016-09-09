ef2df92ca77878437ba556325536fb7207fd6ca5
chrisjsmith01 <chrisjsmith01@gmail.com>
2016-09-09 19:39:51 -0400

    Update Monopoly.py

--- Monopoly.py
+++ Monopoly.py
@@ -1,332 +1,194 @@
-import math
-import string
 import time
 import random
 
 from random import randrange
-playerOne = 0
-playerTwo = 0
-firstChoice = 0
-diceNumber = 0
-turns = 1
-money1 = 1500
-money2 = 1500
-# test comment
-def chance():
-    print "You landed on Chance"
-    global money1, money2, turns
-    chance = randrange (1,5)
-    if chance == 1:
-        player1 = 40
-        print "Advance to Go"
-        print "You have:$ ",money1
-    if chance == 2:
-        print "Bank pays you dividend of $50"
-        money1 += 50
-        print "You have:$ ", money1
-    if chance == 3:
-        print "Advance to St. Charles Place"
-        #player1 = 11
 
-    if chance == 4:
-        print "Advance to Illinois Ave"
-        #player1 = 24
-        if chance == 5:
-            print "Advance to the nearest Utility"
+class Player(object):
+    def __init__(self, player_name):
+        self.player_name = player_name
+        self.board = Board()
+        self.location = 0
+        self.money = 1500
+    def __str__(self):
+        return "Player Class for " + str(self.player_name)
 
-    #if player1 > 25:
-        #player1 = 28;
+    def __repr__(self):
+        return "Player Class for " + str(self.player_name)
 
-    #if player1 < 25)
-        #player1 = 12
-    turns +=1
-def communityChest():
-    global money1, money2, turns
-    print "You landed on Community Chest"
-    commChest = randrange (1,5)
-    if commChest == 1:
-        player1 = 40;
-        print "Advance to Go, Collect $200"
-        money1+=200
-        print "You have:$ ", money1
+    def yourturn(self):
+        print "It's " + self.player_name + " turn to roll the dice"
+        diceOne = randrange(1,6)
+        diceTwo = randrange(1,6)
+        print self.player_name + " rolled a " + str(diceOne) + " and a " + str(diceTwo)
+        time.sleep(2)
+        self.location += (diceOne + diceTwo + self.location)
+        if self.location >= 40:
+            self.location = diceOne + diceTwo
+            self.money += 200
+        property_landed_on = self.board.property_list[self.location]  # a reference to Property class the player landed pn
+        print self.player_name +" landed on " + str(self.board.property_list[self.location])
+        if property_landed_on.is_buyable == True:
+            self.buy_property()
+        time.sleep(2)
+        if diceOne == diceTwo:
+            self.yourturn()
+        if property_landed_on.community_chest:
+            self.community_chest_cards()
+        if property_landed_on.chance:
+            self.chance_cards()
+        if self.location and property_landed_on.is_buyable == 'False':
+            print "You owe " + self.player_name + property_landed_on.rent
+        print str(self.player_name) + " has " + str(self.money)
+    def buy_property(self):
+        purchase = raw_input("Would you like purchase this property?( Type \"yes\" or no\" )")
+        if purchase.lower() == 'yes':
+            self.board.property_list[self.location].is_buyable = False
+            print "It costs " + str(self.board.property_list[self.location].price)
+            if self.money >= self.board.property_list[self.location].price:
+                self.money -= self.board.property_list[self.location].price
 
-    if commChest == 2:
-        print "From sale of stock you get $50"
-        money1 += 50
-        print "You have:$ " ,money1
+        else :
+            print "Fuck you"
+    def community_chest_cards(self):
+        community_chest_random = randrange(1,5)
+        if community_chest_random == 1:
+            print "Life insurance matures. Collect $100"
+            self.money +=100
+        if community_chest_random == 2:
+            print "Bank error in your favor. Collect $200"
+            self.money +=200
+        if community_chest_random == 3:
+            print "Advance to Go. Collect $200"
+            self.money+=200
+            self.location = 39
+        if community_chest_random == 4:
+            print self.player_name + " has won 2nd place at beauty contest. Collect $50"
+            self.money +=50
+        if community_chest_random == 5:
+            print "Christmas fund matures. Collect $100"
+            self.money +=100
+    def chance_cards(self):
+        chance_cards_random = randrange(1,5)
+        if chance_cards_random == 1:
+            print "Take a ride on the Reading Railroad"
+            self.location = 4
+        if chance_cards_random == 2:
+            print "Go back three spaces"
+            self.location -= 3
+        if chance_cards_random == 3:
+            print "Take a walk on the Boardwalk"
+            self.location = 38
+        if chance_cards_random ==4:
+            print "Bank pays you dividend of $50"
+            self.money +=50
+        if chance_cards_random == 5:
+            print "Pay poor tax of $15"
+            self.money-=15
+class ComputerPlayer(Player):
+    def __str__(self):
+        return "Computer Player Class for " + str(self.player_name)
 
+    def __repr__(self):
+        return "Computer Player Class for " + str(self.player_name)
 
-    if commChest == 3:
-        print "You have won second prize in a beauty contest, Collect $10"
-        money1 += 10
-        print "You have:$ ", money1
-
-    if commChest == 4:
-        print "Pay school fees of $150"
-        money1 -= 150
-        print "You have:$ ", money1
-    if commChest == 5:
-        print "Bank error in your favor, Collect $200"
-        money1 += 200
-        print "You have:$ ", money1
-
-    turns +=1
-def buyProperty(cost):
-        global money1
-        global money2
-    #if turns % 2 == 0:
-        purchase = ''
-        print "Would you like to buy this property"
-        time.sleep(1)
-        purchase = raw_input("Type Yes to purchase, type No to not purchase: ")
-        if (purchase.lower() == 'yes'):
-            print "You have purchased this property!"
-            time.sleep(1)
-            money1 -= cost
-            print "You have:$ " ,money1
+    def buy_property(self):
+        purchase = randrange(0,6)
+        if purchase % 2 == 0:
+            print self.player_name + " has purchased this property!"
+            self.money -= self.board.property_list[self.location].price
         else:
-            print "You did not purchase this property."
-            print "You have:$ ", money1
-def landPropertiesPlayerOne():
-    global turns, playerOne, money1
-    if playerOne == 1:
-        medAve = 60
-        print name ,"landed on Mediterranean Avenue($60)"
-        time.sleep(1)
-        buyProperty(medAve)
-        turns +=1
-    if playerOne == 2:
-        communityChest()
-    if playerOne == 3:
-        balticAve = 60
-        print name, "landed on Baltic Avenue($60)"
-        time.sleep(1)
-        buyProperty(balticAve)
-        turns +=1
-    if playerOne == 4:
-        print name, "landed on Income Tax, pay $200"
-        money1 -= 200
-        print "You have:$ ", money1
-        turns +=1
-    if playerOne == 5:
-        readRail = 200
-        print name, "landed on Reading Railroad($200)"
-        time.sleep(1)
-        buyProperty(readRail)
-        turns +=1
-    if playerOne == 6:
-        orientalAve = 100
-        print name, "landed on Oriental Ave($100)"
-        time.sleep(1)
-        buyProperty(orientalAve)
-        turns +=1
-    if playerOne == 7:
-        chance()
-    if playerOne == 8:
-        vermontAve = 100
-        print name, "landed on Vermont Avenue($100)"
-        time.sleep(1)
-        buyProperty(vermontAve)
-        turns +=1
-    if playerOne == 9:
-        connectAve = 120
-        print name,"landed on Connecticut Avenue($120)"
-        time.sleep(1)
-        buyProperty(connectAve)
-        turns +=1
-    if playerOne == 10:
-        print name, "are just visiting jail"
-        turns +=1
-    if playerOne == 11:
-        charlesPlace = 140
-        print name, "landed on St. Charles Place($140)"
-        time.sleep(1)
-        buyProperty(charlesPlace)
-        turns +=1
-    if playerOne == 12:
-        elecCompany = 150
-        print name,"landed on Electric Company($150)"
-        time.sleep(1)
-        buyProperty(elecCompany)
-        turns +=1
-    if playerOne == 13:
-        statesAve = 160
-        print name, "landed on States Avenue($160)"
-        time.sleep(1)
-        buyProperty(statesAve)
-        turns +=1
-    if playerOne == 14:
-        virginiaAve = 160
-        print name,"landed on Virginia Avenue($160)"
-        time.sleep(1)
-        buyProperty(virginiaAve)
-        turns +=1
-    if playerOne == 15:
-        pennRail = 200
-        print name,"landed on Pennsylvania Avenue($200)"
-        time.sleep(1)
-        buyProperty(pennRail)
-        turns +=1
-    if playerOne == 16:
-        jamesPlace = 180
-        print name,"landed on St. James Place($180)"
-        time.sleep(1)
-        buyProperty(jamesPlace)
-        turns +=1
-    if playerOne == 17:
-        communityChest()
-    if playerOne == 18:
-        tennesseeAve = 180
-        print name,"landed on Tennessee Avenue($180)"
-        time.sleep(1)
-        buyProperty(tennesseeAve)
-        turns +=1
-    if playerOne == 19:
-        nyAve = 200
-        print name, "landed on New York Avenue($200)"
-        time.sleep(1)
-        buyProperty(nyAve)
-        turns +=1
-    if playerOne == 20:
-        print name, "landed on Free Parking, you get $500"
-        money1+=500
-        print "You have:$", money1
-    if playerOne == 21:
-        kentuckAve = 220
-        print name, "landed on Kentucky Avenue($220)"
-        time.sleep(1)
-        buyProperty(kentuckAve)
-        turns +=1
-    if playerOne == 22:
-        chance()
-    if playerOne == 23:
-        indianaAve = 220
-        print name, "landed on Indiana Avenue($220)"
-        time.sleep(1)
-        buyProperty(indianaAve)
-        turns +=1
-    if playerOne == 24:
-        illinoisAve = 240
-        print name, "landed on Illinois Avenue($240)"
-        time.sleep(1)
-        buyProperty(illinoisAve)
-        turns +=1
-    if playerOne == 25:
-        boRailroad = 200
-        print name, "landed on B&O Railroad($200)"
-        time.sleep(1)
-        buyProperty(boRailroad)
-        turns +=1
-    if playerOne== 26:
-        atlanticAve = 260
-        print name, "landed on Atlantic Avenue($260)"
-        time.sleep(1)
-        buyProperty(atlanticAve)
-        turns +=1
-    if playerOne == 27:
-        ventnorAve = 260
-        print name, "landed on Ventnor Avenue($260)"
-        time.sleep(1)
-        buyProperty(ventnorAve)
-        turns+=1
-    if playerOne == 28:
-        waterWorks = 150
-        print name,"landed on Water Works($150)"
-        time.sleep(1)
-        buyProperty(waterWorks)
-        turns += 1
-    if playerOne == 29:
-        marvinGardens = 280
-        print name,"landed on Marvin Gardens($280)"
-        time.sleep(1)
-        buyProperty(marvinGardens)
-        turns += 1
-    if playerOne  == 30:
-        print "Go to the Jail"
-    if playerOne == 31:
-        pacificAve = 300
-        print name,"landed on Pacific Avenue($300)"
-        time.sleep(1)
-        buyProperty(pacificAve)
-        turns += 1
-    if playerOne == 32:
-        nocarAve = 300
-        print name,"landed on North Carolina Avenue($300)"
-        time.sleep(1)
-        buyProperty(nocarAve)
-        turns += 1
-    if playerOne == 33:
-        communityChest()
-    if playerOne == 34:
-        pennAve = 320
-        print name,"landed on Pennsylvania Avenue($320)"
-        time.sleep(1)
-        buyProperty(pennAve)
-        turns +=1
-    if playerOne == 35:
-        shortLine = 200
-        print name,"landed on Short Line($200)"
-        time.sleep(1)
-        buyProperty(shortLine)
-        turns +=1
-    if playerOne == 36:
-        chance()
-    if playerOne == 37:
-        parkPlace = 350
-        print name,"landed on Park Place($350)"
-        time.sleep(1)
-        buyProperty(parkPlace)
-        turns +=1
-    if playerOne == 38:
-        print name, "landed on Luxury Tax, lose $100"
-        money1-=100
-    if playerOne == 39:
-        boardwalk = 400
-        print name, "landed on Boardwalk($400)"
-        time.sleep(1)
-        buyProperty(boardwalk)
-        turns+=1
-    if playerOne == 40:
-        print name,"landed on Go, collect $400"
-        money1+=400
-    if playerOne > 40:
-        playerOne = 0
-def playerOneTurn():
-    global playerOne
-    if playerOne <40:
-        print "It's",name, "turn to roll"
-    diceNumber = randrange(1,12)
-    #diceNumber = 1
-    playerOne += diceNumber
-    if playerOne < 40:
-        print "Your landed on: ", playerOne
-        time.sleep(1)
+            print self.player_name + " did not buy this property!"
+class Property(object):
+    def __init__(self, name, price, is_buyable, community_chest, chance, land_price):
+        self.name = name
+        self.price = price
+        self.owner = ""
+        self.is_buyable = is_buyable
+        self.community_chest  = community_chest
+        self.chance = chance
+        self.land_price = land_price
+    def __repr__(self):
+        return self.name
 
-    landPropertiesPlayerOne()
+class Board(object): # only one instance of this
+    def __init__(self):
+        self.player_list = []  # stores the list of players, in their turn order
+        self.property_list = [] # stores a list of properties (instances of Property class)
+        self.populate_board()  # make property_list have values
 
+    def add_player(self, player_name):
+        self.player_list.append(Player(player_name))
 
-def goesFirst():
-    global turns
-    #rollOne = randrange(1, 12)
-    #rollTwo = randrange(1,12)
-    #if rollOne > rollTwo :
-    while (turns > 0):
-     playerOneTurn()
-        #turns = 2
+    def add_computer_players(self, num_of_computer_players):
+        for _ in range(num_of_computer_players):
+            # Run this loop num_of_computer_players time
+            random_names = ['Thimble', 'Battleship', 'Shoe', 'Race Car', 'Wheel Barrel']
+            self.player_list.append(ComputerPlayer(random.choice(random_names)))
+
+        print self.player_list
+
+    def populate_board(self):
+        self.property_list.append(Property("Mediterranean Avenue", 60, True, False, False, 2))
+        self.property_list.append(Property("Community Chest", 0, False, True, False, 0))
+        self.property_list.append(Property("Baltic Avenue", 60, True, False, False, 4))
+        self.property_list.append(Property("Income Tax, lose $100", -100, False, False, False, 0))
+        self.property_list.append(Property("Reading Railroad", 200, True, False, False, 0))
+        self.property_list.append(Property("Oriental Avenue", 100, True, False, False, 7))
+        self.property_list.append(Property("Chance", 0, False, False, True, 0))
+        self.property_list.append(Property("Vermont Avenue", 100, True, False, False, 9))
+        self.property_list.append(Property("Connecticut Avenue", 120, True, False, False, 10))
+        self.property_list.append(Property("just visiting Jail", 0, False, False, False, 0))
+        self.property_list.append(Property("St. Charles Place", 140, True, False, False, 12))
+        self.property_list.append(Property("Electric Company", 150, True, False, False, 0))
+        self.property_list.append(Property("States Avenue", 160, True, False, False, 14))
+        self.property_list.append(Property("Virginia Avenue", 160, True, False, False, 14))
+        self.property_list.append(Property("Pennsylvania Railroad", 200, True, False, False, 0))
+        self.property_list.append(Property("St. James Place", 180, True, False, False, 17))
+        self.property_list.append(Property("Community Chest", 0, False, True, False, 0))
+        self.property_list.append(Property("Tennessee Avenue", 180, True, False, False, 19))
+        self.property_list.append(Property("New York Avenue", 200, True, False, False, 20))
+        self.property_list.append(Property("Free Parking", 0, False, False, False, 0))
+        self.property_list.append(Property("Kentucky Avenue", 220, True, False, False, 22))
+        self.property_list.append(Property("Chance", 0, False, False, True, 0))
+        self.property_list.append(Property("Indiana Avenue", 220, True, False, False, 24))
+        self.property_list.append(Property("Illinois Avenue", 240, True, False, False,25))
+        self.property_list.append(Property("B&O Railroad", 200, True, False, False, 0))
+        self.property_list.append(Property("Atlantic Avenue", 260, True, False, False, 27))
+        self.property_list.append(Property("Ventnor Avenue", 260, True, False, False, 28))
+        self.property_list.append(Property("Water Works", 150, True, False, False, 0))
+        self.property_list.append(Property("Marvin Gardens", 280, True, False, False, 30))
+        self.property_list.append(Property("Go to Jail", 0, False, False, False, 0))
+        self.property_list.append(Property("Pacific Avenue", 300, True, False, False, 32))
+        self.property_list.append(Property("North Carolina Avenue", 300, True, False, False, 33))
+        self.property_list.append(Property("Community Chest", 0, False, True, False, 0))
+        self.property_list.append(Property("Pennsylvania Avenue", 320, True, False, False, 35))
+        self.property_list.append(Property("Short Line", 200, True, False, False, 0))
+        self.property_list.append(Property("Chance", 0, False, False, True, 0))
+        self.property_list.append(Property("Park Place", 350, True, False, False, 38))
+        self.property_list.append(Property("Luxury Tax, lose $100", -100, False, False, False, 0))
+        self.property_list.append(Property("Boardwalk", 400, True, False, False, 40))
+        self.property_list.append(Property("Go, collect $200", 0, False, False, False, 0))
 
 def main():
-   global name
-   print "Welcome to Monopoly"
-   name = raw_input("Enter your name: ")
-   startGame = raw_input("Type Start to begin the game: ")
-   if startGame == 'Start':
-     goesFirst()
-   elif startGame == 'start':
-     goesFirst()
-   else :
-      main()
-
-
-
+    global numPlay
+    board = Board()
+    print "Welcome to Monopoly"
+    human_player_name = raw_input("Enter your name: ")
+    while True:
+        numPlay = input("How many players (Max 4): ")
+        if numPlay >=2 and numPlay <=4:
+            break
+        else:
+            numPlay = input("Please enter a valid player number! (2-4)")
+    while True:
+        startGame = raw_input("Type Start to begin the game: ")
+        if startGame.lower() == 'start':
+            board.add_player(human_player_name)
+            board.add_computer_players(numPlay - 1)
+            break
+    while True:
+        for player in board.player_list:
+            player.yourturn()
+            time.sleep(2)
 
 main()
